14d1888220e50364e06c0f1c4af740f48edcdf87
ANSDANGI <anshudangi72@gmail.com>
2023-12-26 12:59:17 +0530

    variable declaration
    variable declaration

---
+++ 01_swapVariable.js
@@ -0,0 +1,11 @@
+var a="10";
+var b="20";
+// before swapping
+console.log("a value->",a);
+console.log("b value->",b);
+var temp=a;
+a=b;
+b=temp;
+// after swapping
+console.log("a value->",a);
+console.log("b value->",b);
---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+console.log("Hello JS , I am Anshu");
+//   variable declaration
+var num;
+// variable intialization
+num="10"; 
+console.log(num);
+// variable updation
+num="20";
+console.log(num); 
+var name="Anshu Dangi";
+console.log("My Full Name:",name);
+var ans;
+console.log(ans);
+ans=null;
+console.log(ans);
