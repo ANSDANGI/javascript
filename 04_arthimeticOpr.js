4765f577157e0a1fe2907defbd930107558b4867
ANSDANGI <anshudangi72@gmail.com>
2023-12-31 17:46:18 +0530

    operator
    operator

---
+++ 04_arthimeticOpr.js
@@ -0,0 +1,42 @@
+var n1=7;
+var n2=2;
+var result=n1+n2;
+console.log(` +==>Addition is ${result}`);
+var result=n1-n2;
+console.log(`-====>substraction is:${result}`);
+var result=n1*n2;
+console.log(`====>Multipaction is:${result}`);
+var result=n1%n2;
+console.log(`====>remainder is:${result}`);
+var result=n1/n2;
+console.log(`-====>Division  is:${result}`);
+var result=n1**n2;
+console.log(`-====>Exponention is:${result}`);
+var a=7;
+// a++; a+1;    
+a++;
+++a;
+console.log(a);
+var num=10;
+num+=2;
+// compound addition; num=num+2;
+var n=8;
+var h=n%2==0?"Even":"Odd";
+console.log(h);
+var a=10;
+var b="10";
+console.log(typeof b);
+console.log(typeof a);
+console.log(a==b);
+// double equal its just check value not data type note point
+var n1=100;
+var n2="100";
+console.log(n1==n2);
+console.log(n1===n2); 
+// it will compare the value or datatype;
+var c=2;
+var d=2;
+console.log(c!=d);
+
+
+
---
+++ 04_operator.js
@@ -0,0 +1,9 @@
+var num=5;
+var result=num%2==0?"Given number is even":"Given number is odd";
+console.log(result);
+var n1="100";
+var n2=100;
+console.log(`comparison using== operator =====>${n1==n2}`);
+console.log(`comparison using=== operator =====>${n1===n2}`);
+
+
