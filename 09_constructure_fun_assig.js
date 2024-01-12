af2a03d9081d04a53ea79fdae053675c2649fa4f
ANSDANGI <anshudangi72@gmail.com>
2024-01-12 16:10:19 +0530

    fun
    fun

--- 09_constructure_fun_assig.js
+++ 09_constructure_fun_assig.js
@@ -3,20 +3,33 @@ function Bank(bankName,location,ifscCode,branchCode,openTime,closeTime ){
      this.location=location;
      this.ifscCode=ifscCode;
      this.branchCode=branchCode;
-     this.openTime=openTime;
-     this.closeTime=closeTime;
+     this.getDetails=function(){
+        console.log(`bank name:${this.bankName},location:${this.location},ifsccode:${this.ifscCode},branchcode:${this.branchCode}`);
+     }
+     
      
 }
-const a=new Bank("YES  BANK","pune","yES123",1234,"9 AM IST","6 PM IST");
-console.log(a);
-const a1=new Bank("SBI Bank","pune","sbi123",1222,"9 AM IST","6 PM IST");
-console.log(a1);
+const a=new Bank("YES  BANK","pune","yES123",1234,);
+
+
+const a1=new Bank("SBI Bank","pune","sbi123",1222,);
+
 const a2=new Bank("MAHINDRA  BANK","pune","mah123",1777);
-console.log(a2);
-const a3=new Bank("Axis BANK","pune","axis123",1266,"9 AM IST","6 PM IST");
-console.log(a3);
+
+const a3=new Bank("Axis BANK","pune","axis123",1266);
+a.getDetails();
+a1.getDetails();
+a2.getDetails();
+a3.getDetails();
+
+
+
 console.log("===============");
-console.log("open time of SBI is:",a1.openTime,"closing time of SBI",a1.closeTime);
-console.log("Bank Name:",a3.bankName,"open time of axisbank is:",a3.openTime,"closing time of axisbank",a3.closeTime);
-console.log("Bank Name:",a.bankName,"open time of axisbank is:",a.openTime,"closing time of axisbank",a.closeTime,"branch code:",a.branchCode);
+console.log("=====================================================prototype");
+Bank.prototype.openTime="9 AM IST";
+Bank.prototype.closeTime="6 PM IST"
+console.log(`SBI Bank:${a1.bankName} opening time is:${a1.openTime} and closing time is:${a1.closeTime}`);
+console.log(`Axis Bank:${a3.bankName} opening time is:${a3.openTime} and closing time is:${a3.closeTime}`);
+console.log(`Yes Bank:${a.bankName},branch code:${a.branchCode} opening time is:${a.openTime} and closing time is:${a.closeTime}`);
+
 
